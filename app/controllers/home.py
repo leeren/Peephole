b5e8bd5e82ac6acdc45b6496276b583e02e66ab3
leeren <leeren@berkeley.edu>
2016-11-11 22:13:30 -0800

    Made some changes

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -6,10 +6,9 @@ mod=Blueprint('home', __name__)
 @mod.route('/', methods=['GET', 'POST'])
 def index():
     form = LoginForm()
-    user = {'name':'Leeren'}
     if form.validate_on_submit():
         flash('Login requested for OpenID="%s", remember_me=%s' % (form.openid.data, str(form.remember_me.data)))
-    return render_template('login.html', form=form, user=user)
+    return render_template('login.html', form=form)
 
 @mod.errorhandler(403)
 def page_not_found(e):
