09f7c566694b13732e6bb72a3e5cc3a1c8e7515f
leeren <leeren@berkeley.edu>
2016-11-12 21:43:10 -0800

    Random transactions etc

--- app/controllers/views.py
+++ app/controllers/views.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, render_template, flash, redirect
-from app.forms import LoginForm
-from app import lm
+from flask import Blueprint, render_template, flash, redirect, session, request, url_for, g
+from app import app, lm, facebook
 from flask_oauthlib.client import OAuthException
 from flask_login import current_user, login_user, logout_user, login_required
 
 mod=Blueprint('home', __name__)
 
-@lm.user_loader(user_id):
+@lm.user_loader
+def load_user(user_id):
     return User.query.get(int(user_id))
 
 @app.before_request
@@ -15,14 +15,53 @@ def get_user():
 
 @mod.route('/', methods=['GET', 'POST'])
 def index():
-    form = LoginForm()
     if g.user.is_authenticated:
-        return render_template('tmp.html')
+        return render_template('define-self.html')
     return render_template('login.html')
 
-@mod.route('/define-self', methods=['GET'])
-def define_self():
-	return render_template('define-self.html')
+@mod.route('/login')
+def login():
+    callback = url_for(
+            'authorize_facebook',
+            next=request.args.get('next') or request.referrer or None,
+            _external=True
+            )
+    return facebook.authorize(callback=callback)
+
+@app.route('/login/authorize')
+def authorize_facebook():
+    resp = facebook.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error%s' % (
+                request.args['error_reason'],
+                request.args['error_description']
+                )
+        if isinstance(resp, OAuthException):
+            return 'Acess denied: %s' % resp.message
+
+        session['oauth_token'] = (resp['access_token'], '')
+        user = facebook.get('/me/?fields=email,name,id,picture')
+        return set_user(user)
+
+def set_user(user):
+    user = User.query.filter_by(facebook_id = user.data['id']).first()
+    if user is None:
+        create_user(user)
+        return render_template('define-self.html')
+    login_user(user, remember=True)
+    return render_template('define-self.html')
+    
+def create_user(user):
+    new_user = User(
+        facebook_id = user.data['id'],
+        pic = user.data['picture']['data']['url'],
+        name = user.data['name'],
+        email = user.data['email']
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    login_user(new_user, remember=True)
+    return new_user
 
 @mod.errorhandler(403)
 def page_not_found(e):
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,39 +1,21 @@
 from app import db
-from flask_security import UserMixin, RoleMixin
-
-role_users = db.Table('role_users',
-        db.Column('user_id', db.Integer(), db.ForeignKey('users.id')),
-        db.Column('role_id', db.Integer(), db.ForeignKey('roles.id')))
-
-class Role(db.Model, RoleMixin):
-    
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
+from flask_security import UserMixin
 
 class User(db.Model, UserMixin):
 
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    facebook_id = db.Column(db.String)
+    pic_url = db.Column(db.String)
+    about_me = db.Column(db.String)
+    name = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    last_login_at = db.Column(db.DateTime())
-    current_login_at = db.Column(db.DateTime())
-    last_login_ip = db.Column(db.String(100))
-    current_login_ip = db.Column(db.String(100))
-    login_count = db.Column(db.Integer)
-    roles = db.relationship('Role', secondary=role_users, backref=db.backref('users', lazy='dynamic'))
     comments = db.relationship('Comment', backref='commenter', lazy='dynamic', foreign_keys='Comment.commenter_id')
     received_comments = db.relationship('Comment', backref='received', lazy='dynamic', foreign_keys='Comment.receiver_id')
     rated_stats = db.relationship('Stat', backref='rater', lazy='dynamic', foreign_keys='Stat.rater_id')
     user_stats = db.relationship('Stat', backref='user', lazy='dynamic', foreign_keys='Stat.user_id')
     votes = db.relationship('Vote', backref='user', lazy='dynamic')
-    roles = db.relationship('Role', secondary=role_users, backref='user', lazy='dynamic')
-    connections = db.relationship('Connection', backref='user', lazy='joined')
 
     def __repr__(self):
         return 'User: %r' % self.email
@@ -70,19 +52,3 @@ class Vote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-
-class Connection(db.Model):
-
-    __tablename__ = "connections"
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    provider_id = db.Column(db.String(255))
-    provider_user_id = db.Column(db.String(255))
-    access_token = db.Column(db.String(255))
-    secret = db.Column(db.String(255))
-    display_name = db.Column(db.String(255))
-    full_name = db.Column(db.String(255))
-    profile_url = db.Column(db.String(512))
-    image_url = db.Column(db.String(512))
-    rank = db.Column(db.Integer)
